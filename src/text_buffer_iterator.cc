3241b564995493c6ce60cb042797f068b4a2e354
papieros <robertrugala@gmail.com>
2026-02-06 20:52:06 +0100

    implemented text_buffer_iterator

---
+++ src/text_buffer_iterator.cc
@@ -0,0 +1,43 @@
+#include "buffer.hpp"
+#include "utf8.hpp"
+namespace bed {
+using tit = TextBuffer::text_buffer_iterator;
+tit::text_buffer_iterator(const std::vector<TextBuffer::Line>* lines)
+    : m_lines(lines)
+    , m_sz(lines->size()) {};
+tit tit::end(const std::vector<TextBuffer::Line>* lines)
+{
+    auto t = text_buffer_iterator(lines);
+    t.m_line = t.m_sz;
+    return t;
+}
+const TextBuffer::char_t& tit::operator*() const
+{
+    return (*m_lines)[m_line].contents[m_col];
+}
+void tit::operator++()
+{
+    if (++m_col >= (*m_lines)[m_line].contents.size()) {
+        m_line++;
+        m_col = 0;
+    }
+}
+void tit::operator++(int)
+{
+    ++(*this);
+}
+bool tit::operator==(const tit& other) const
+{
+    if (m_lines != other.m_lines)
+        return false;
+    return (other.m_line >= m_sz && m_line >= m_sz) || (other.m_line == m_line && other.m_col == m_col);
+}
+bool tit::operator!=(const tit& other) const
+{
+    return !(*this == other);
+}
+TextBuffer::Cursor tit::current_cursor_pos(void) const
+{
+    return TextBuffer::Cursor { (long)m_line, (long)m_col };
+}
+}
